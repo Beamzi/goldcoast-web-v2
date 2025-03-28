c7234419361f17ccbaa9190c145c7499b752484b
James Day <159420270+Beamzi@users.noreply.github.com>
2025-03-29 09:44:55 +1000

    loaded and modified several components, basic architecture set up and stack

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+import { AuroraBackground } from "@/components/ui/aurora-background";
+import { Showcase } from "@/components/ui/Showcase";
+import HeroSectionOne from "@/components/HeroSection";
+
+export default function Home() {
+  return (
+    <>
+      <HeroSectionOne></HeroSectionOne>
+    </>
+  );
+}
